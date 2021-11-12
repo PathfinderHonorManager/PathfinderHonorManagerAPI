cbfdcd9a518f45f94b549fe94d29b4ca3bf47c1b
bcummings <bcummings@cardlytics.com>
2021-11-11 21:25:05 -0500

    added pathfinderhonorid to dto

--- PathfinderHonorManager/Dto/Outgoing/PathfinderHonorDto.cs
+++ PathfinderHonorManager/Dto/Outgoing/PathfinderHonorDto.cs
@@ -4,10 +4,10 @@ namespace PathfinderHonorManager.Dto.Outgoing
 {
     public class PathfinderHonorDto
     {
+        public Guid PathfinderHonorID { get; set; }
         public Guid PathfinderID { get; set; }
         public Guid HonorID { get; set; }
         public String Name { get; set; }
         public string Status { get; set; }
     }
 }
-
