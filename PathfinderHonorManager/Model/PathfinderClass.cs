e9e363f5678c5b290c5ec2dacdc7314e35ff682e
bcummings <bcummings@cardlytics.com>
2022-02-03 22:44:57 -0500

    Added classname lookup

---
+++ PathfinderHonorManager/Model/PathfinderClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PathfinderHonorManager.Model
+{
+    [Table("pathfinder_class")]
+    public class PathfinderClass
+    {
+        [Key, Column("grade")]
+        public int Grade { get; set; }
+        [Column("name")]
+        public String ClassName { get; set; }
+    }
+}
---
+++ PathfinderHonorManager/Pathfinder-DB/Data/public.pathfinder_class_data.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO public.pathfinder_class VALUES (5, 'Friend');
+INSERT INTO public.pathfinder_class VALUES (6, 'Companion');
+INSERT INTO public.pathfinder_class VALUES (7, 'Explorer');
+INSERT INTO public.pathfinder_class VALUES (8, 'Ranger');
+INSERT INTO public.pathfinder_class VALUES (9, 'Voyager');
+INSERT INTO public.pathfinder_class VALUES (10, 'Guide');
+INSERT INTO public.pathfinder_class VALUES (11, 'Guide');
+INSERT INTO public.pathfinder_class VALUES (12, 'Guide');
