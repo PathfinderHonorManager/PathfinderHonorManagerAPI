9044e39d7ea9a67c2609a7044398efb6c643b875
Brian Cummings <brian.m.cummings@gmail.com>
2023-03-12 10:27:43 -0400

    Fixed URL validation on HonorValidator

--- PathfinderHonorManager/Validators/HonorValidator.cs
+++ PathfinderHonorManager/Validators/HonorValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using System;
+using FluentValidation;
 using PathfinderHonorManager.Dto.Incoming;
 
 namespace PathfinderHonorManager.Validators
@@ -9,8 +10,17 @@ namespace PathfinderHonorManager.Validators
         {
             RuleFor(h => h.Name).NotEmpty();
             RuleFor(h => h.Level).InclusiveBetween(1, 3);
-            RuleFor(h => h.WikiPath).NotEmpty().WithMessage("WikiPath must be a valid URL.");
-            RuleFor(h => h.PatchFilename).NotEmpty().Matches(@"\.(gif|jpe?g|png)$").WithMessage("PatchFilename must be a gif, jpg or png.");
+            RuleFor(h => h.WikiPath)
+                .Must(IsValidUri)
+                .WithMessage("'WikiPath' must be a valid URL.");
+            RuleFor(h => h.PatchFilename).NotEmpty().Matches(@"\.(gif|jpe?g|png)$")
+                .WithMessage("'PatchFilename' must be a gif, jpg or png.");
+        }
+
+        private bool IsValidUri(Uri uri)
+        {
+            return Uri.TryCreate(uri.ToString(), UriKind.Absolute, out Uri result)
+                && (result.Scheme == Uri.UriSchemeHttp || result.Scheme == Uri.UriSchemeHttps);
         }
     }
 }
