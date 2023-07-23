81c5b8944b5099d4cfd4473960d11338d81c0be5
Brian Cummings <brian.m.cummings@gmail.com>
2023-07-23 11:45:32 -0400

    Made it easier to create a local db with seed data

--- PathfinderHonorManager/Pathfinder-DB/Data/public.pathfinder_honor_status_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.pathfinder_honor_status VALUES (100, 'Planned');
-INSERT INTO public.pathfinder_honor_status VALUES (200, 'Earned');
-INSERT INTO public.pathfinder_honor_status VALUES (300, 'Awarded');
