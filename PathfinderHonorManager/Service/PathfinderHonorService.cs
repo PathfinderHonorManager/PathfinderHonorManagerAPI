97db07b856c69ef382c17766693184a8e6d9fcb9
Brian Cummings <brian.m.cummings@gmail.com>
2023-11-24 13:36:22 -0500

    Added earned calculation to post

--- PathfinderHonorManager/Service/PathfinderHonorService.cs
+++ PathfinderHonorManager/Service/PathfinderHonorService.cs
@@ -99,6 +99,11 @@ namespace PathfinderHonorManager.Service
 
             var newEntity = _mapper.Map<PathfinderHonor>(newPathfinderHonor);
 
+            if (newPathfinderHonor.Status == HonorStatus.Earned.ToString())
+            {
+                newEntity.Earned = DateTime.UtcNow;
+            }
+
             await _dbContext.AddAsync(newEntity, token);
             await _dbContext.SaveChangesAsync(token);
             _logger.LogInformation($"Pathfinder honor(Id: {newEntity.PathfinderHonorID} added to database.");
