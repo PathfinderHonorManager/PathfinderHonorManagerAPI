1bfba159f9d8c2565af65cba789df60f5fcb978e
Brian Cummings <brian-cummings@users.noreply.github.com>
2024-01-29 14:32:13 -0500

    Update CreateSchemata.sql
    Fixed typos

--- PathfinderHonorManager/Pathfinder-DB/CreateSchemata.sql
+++ PathfinderHonorManager/Pathfinder-DB/CreateSchemata.sql
@@ -14,26 +14,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 3658 (class 1262 OID 16504)
--- Name: pathfinder; Type: DATABASE; Schema: -; Owner: postgres
---
-
-ALTER DATABASE pathfinder OWNER TO postgres;
-
-\connect pathfinder
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
 -- TOC entry 5 (class 2615 OID 2200)
 -- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
 --
