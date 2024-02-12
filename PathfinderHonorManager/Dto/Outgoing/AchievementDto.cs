fc2e9089dd4035fd2ecd1ee612206adabe45d8b5
Brian Cummings <brian.cummings@gmail.com>
2024-02-11 19:59:41 -0500

    Added achievement sequence to dto

--- PathfinderHonorManager/Dto/Outgoing/AchievementDto.cs
+++ PathfinderHonorManager/Dto/Outgoing/AchievementDto.cs
@@ -8,6 +8,7 @@ namespace PathfinderHonorManager.Dto.Outgoing
         public Guid AchievementID { get; set; }
         public int Level { get; set; }
         public string LevelName { get; set; }
+        public int AchievementSequenceOrder { get; set; }
         public string ClassName { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
