53cb896b959e75527a088eceb627861deae8f954
Brian Cummings <brian.cummings@gmail.com>
2025-01-01 22:27:03 -0500

    Added additional tests

--- PathfinderHonorManager.Tests/Validator/PathfinderHonorValidatorTests.cs
+++ PathfinderHonorManager.Tests/Validator/PathfinderHonorValidatorTests.cs
@@ -95,6 +95,155 @@ namespace PathfinderHonorManager.Tests.Validator
                 .WithSeverity(Severity.Error);
         }
 
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public async Task Validate_ValidStatusCode_ShouldPass(int statusCode)
+        {
+            var newPathfinderHonor = new Incoming.PathfinderHonorDto
+            {
+                HonorID = Guid.NewGuid(),
+                PathfinderID = Guid.NewGuid(),
+                StatusCode = statusCode
+            };
+
+            var validationResult = await _pathfinderHonorValidator
+                .TestValidateAsync(newPathfinderHonor);
+
+            validationResult.ShouldNotHaveValidationErrorFor(p => p.StatusCode);
+        }
+
+        [Test]
+        public async Task Validate_DuplicateHonorAssignment_ValidationError()
+        {
+            using (var context = new PathfinderContext(ContextOptions))
+            {
+                // Create and save test data
+                var pathfinderId = Guid.NewGuid();
+                var honorId = Guid.NewGuid();
+                var existingHonor = new PathfinderHonor
+                {
+                    PathfinderID = pathfinderId,
+                    HonorID = honorId,
+                    StatusCode = 1,
+                    Created = DateTime.UtcNow
+                };
+                await context.PathfinderHonors.AddAsync(existingHonor);
+                await context.SaveChangesAsync();
+
+                var newPathfinderHonor = new Incoming.PathfinderHonorDto
+                {
+                    HonorID = honorId,
+                    PathfinderID = pathfinderId,
+                    StatusCode = 1
+                };
+
+                var validationResult = await _pathfinderHonorValidator
+                    .TestValidateAsync(newPathfinderHonor, options =>
+                    {
+                        options.IncludeRuleSets("post");
+                    });
+
+                validationResult.ShouldHaveValidationErrorFor(p => p.HonorID)
+                    .WithSeverity(Severity.Error);
+            }
+        }
+
+        [Test]
+        public async Task Validate_MultipleValidationRules_ValidationError()
+        {
+            var newPathfinderHonor = new Incoming.PathfinderHonorDto
+            {
+                HonorID = Guid.Empty,
+                PathfinderID = Guid.Empty,
+                StatusCode = -1
+            };
+
+            var validationResult = await _pathfinderHonorValidator
+                .TestValidateAsync(newPathfinderHonor, options =>
+                {
+                    options.IncludeRuleSets("post");
+                    options.IncludeRulesNotInRuleSet();
+                });
+
+            validationResult.ShouldHaveValidationErrorFor(p => p.StatusCode);
+            validationResult.ShouldHaveValidationErrorFor(p => p.HonorID);
+            validationResult.ShouldHaveValidationErrorFor(p => p.PathfinderID);
+        }
+
+        [Test]
+        public async Task Validate_StatusTransition_ValidationError()
+        {
+            using (var context = new PathfinderContext(ContextOptions))
+            {
+                // Create initial honor with Planned status
+                var pathfinderId = Guid.NewGuid();
+                var honorId = Guid.NewGuid();
+                var existingHonor = new PathfinderHonor
+                {
+                    PathfinderID = pathfinderId,
+                    HonorID = honorId,
+                    StatusCode = 1, // Planned
+                    Created = DateTime.UtcNow
+                };
+                await context.PathfinderHonors.AddAsync(existingHonor);
+                await context.SaveChangesAsync();
+
+                // Try to create duplicate with different status
+                var newPathfinderHonor = new Incoming.PathfinderHonorDto
+                {
+                    HonorID = honorId,
+                    PathfinderID = pathfinderId,
+                    StatusCode = 2 // Earned
+                };
+
+                var validationResult = await _pathfinderHonorValidator
+                    .TestValidateAsync(newPathfinderHonor, options =>
+                    {
+                        options.IncludeRuleSets("post");
+                    });
+
+                validationResult.ShouldHaveValidationErrorFor(p => p.HonorID)
+                    .WithSeverity(Severity.Error);
+            }
+        }
+
+        [Test]
+        public async Task Validate_NonexistentPathfinder_ValidationError()
+        {
+            using (var context = new PathfinderContext(ContextOptions))
+            {
+                var honorId = Guid.NewGuid();
+                var honor = new Honor
+                {
+                    HonorID = honorId,
+                    Name = "Test Honor",
+                    Level = 1,
+                    PatchFilename = "test.jpg",
+                    WikiPath = new Uri("https://example.com")
+                };
+                await context.Honors.AddAsync(honor);
+                await context.SaveChangesAsync();
+
+                var newPathfinderHonor = new Incoming.PathfinderHonorDto
+                {
+                    HonorID = honorId,
+                    PathfinderID = Guid.NewGuid(), // Non-existent pathfinder
+                    StatusCode = 1,
+                    Status = "Planned"
+                };
+
+                var validationResult = await _pathfinderHonorValidator
+                    .TestValidateAsync(newPathfinderHonor, options =>
+                    {
+                        options.IncludeRuleSets("post");
+                    });
+
+                validationResult.ShouldHaveValidationErrorFor(p => p.PathfinderID)
+                    .WithSeverity(Severity.Error);
+            }
+        }
+
         public static void SeedDatabase(PathfinderContext context)
         {
             context.Database.EnsureDeleted();
