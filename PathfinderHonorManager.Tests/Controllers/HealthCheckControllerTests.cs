a23e5806d70bfe6ad5947039c9e8423ec7e9c48b
Brian Cummings <brian.cummings@gmail.com>
2025-06-15 17:13:47 -0400

    Simplied tests to remove 503 test

--- PathfinderHonorManager.Tests/Controllers/HealthCheckControllerTests.cs
+++ PathfinderHonorManager.Tests/Controllers/HealthCheckControllerTests.cs
@@ -1,14 +1,7 @@
-using System;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
-using PathfinderHonorManager.DataAccess;
-using System.Net;
-using PathfinderHonorManager.Healthcheck;
-using Microsoft.AspNetCore.Hosting;
-using System.Linq;
 
 namespace PathfinderHonorManager.Tests.Controllers
 {
@@ -16,41 +9,12 @@ namespace PathfinderHonorManager.Tests.Controllers
     public class HealthCheckControllerTests
     {
         [Test]
-        public async Task HealthCheck_WhenDatabaseIsHealthy_ReturnsOk()
+        public async Task HealthCheck_WhenHealthy_ReturnsOk()
         {
             await using var factory = new WebApplicationFactory<Startup>();
             var client = factory.CreateClient();
             var response = await client.GetAsync("/health");
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-        }
-
-        [Test]
-        public async Task HealthCheck_WhenDatabaseIsUnhealthy_ReturnsServiceUnavailable()
-        {
-            await using var factory = new CustomWebApplicationFactory();
-            var client = factory.CreateClient();
-            var response = await client.GetAsync("/health");
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.ServiceUnavailable));
-        }
-    }
-
-    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
-    {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices(services =>
-            {
-                // Remove the existing PathfinderContext registration
-                var descriptor = services.SingleOrDefault(
-                    d => d.ServiceType == typeof(DbContextOptions<PathfinderContext>));
-                if (descriptor != null)
-                {
-                    services.Remove(descriptor);
-                }
-                // Add a PathfinderContext with an invalid connection string
-                services.AddDbContext<PathfinderContext>(options =>
-                    options.UseNpgsql("Host=invalid;Port=5432;Database=test;Username=test;Password=test"));
-            });
         }
     }
 } 
